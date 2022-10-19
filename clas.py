ab04a4865247037d2a537be20fc077f3917c6dc8
prateek <agarwalprateek666@gmail.com>
2022-10-19 10:01:19 +0530

    python

---
+++ clas.py
@@ -0,0 +1,17 @@
+mobiles = ["iphone","samsung","vivo","oneplus"]
+mobiles.append("oppo")
+print(mobiles)
+
+groceryItems = ["milk","dahi","suji","rice","wheat"]
+for grocery in groceryItems:
+    print(grocery)
+
+mystr = "Prateek is a good boy"
+print(mystr[0:78])
+print(len(mystr))
+print(mystr[0:-2])
+name="prateek"
+firstchar = name[0]
+lastchar = name[-1]
+nameBody = name[1:-1]
+print(firstchar,lastchar,nameBody)
